827c4ce091493e92244e37e72c02f8dd89ba3e4e
sam <samdysch@hotmail.co.uk>
2015-11-09 18:57:30 +0000

    Checks for Gillespie

---
+++ sheet_3/conditional_absorb_new_initial_conditions/gillespie.cc
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <fstream>
+
+double get_random() {// uniform random number between 0 and 1
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<> dis(0., 1.);
+	return dis(gen);
+}
+
+double get_random_expo(const double lambda) {// random number from exponential distribution with parameter lambda
+	return -1./lambda * log(get_random());
+}
+
+bool absorbed_beginning(bool* pos, const int length) {// determine if the particle has been absorbed @ n=0
+	for (int n = 0; n < length; ++n) {
+		if (pos[n] && n == 0) {// if particle position is beginning, been absorbed
+			return true;
+		}
+	}
+	return false;
+}
+
+bool absorbed_end(bool* pos, const int length) {// determine if the particle has been absorbed @ n=N
+	for (int n = 0; n < length; ++n) {
+		if (pos[n] && n == length - 1) {// if particle position is end, been absorbed
+			return true;
+		}
+	}
+	return false;
+}
+
+void initial_conditions(bool* pos, const int length, const int ni, double& time) {// initialise array to initial conditions
+	time = 0;
+	for (int n = 0; n < length; ++n) {// initialise initial conditions -> position[i] = delta(i,n_initial)
+		if (n == ni) {
+			pos[n] = true;
+		}
+		else {
+			pos[n] = false;
+		}
+	}
+}
+
+int shuffle(bool* pos, const int length, double& time) {// determines possible states to move to, increments time, returns position of new state
+	const double rate = 0.05;// transition rate, W+- fixed @ 0.05
+	const double lambda = rate + rate;
+	time += get_random_expo(lambda);// increment time drawn from expo distribution
+	int current_position;
+	for (int n = 0; n < length; ++n) {// get current position
+		if(pos[n]) {
+			current_position = n;
+		}
+	}
+	const double p = get_random();// random number between 0 and 1
+	if (p < rate / lambda) {// p < 1/2 in default case
+		return current_position + 1;
+	}
+	else return current_position - 1;
+}
+
+int main() {
+	const int runs = 5000;// number of gillespie simulations to run
+	const int N = 100;
+	const int initial_n = N / 2;// initial position 
+	double t = 0;// start at 0 time
+	bool positions[N];// array of positions
+
+	std::ofstream abs_end;
+	abs_end.open("absorbed_end.dat");
+	std::ofstream abs;
+	abs.open("absorbed.dat");
+
+	for (int i = 0; i < runs; ++i) {
+		initial_conditions(positions, N, initial_n, t);
+		while (true) {	
+			if (!absorbed_beginning(positions, N) && !absorbed_end(positions, N)) {
+				const int new_position = shuffle(positions, N, t);
+				for (int n = 0; n < N; ++n) {// change positions over
+					if(positions[n]) {
+						positions[n] = false;
+					}
+				}
+				positions[new_position] = true;
+				//std::cout << new_position << std::endl;
+			}
+			else {
+				abs << t << std::endl;
+				if (absorbed_end(positions, N)) {
+					abs_end << t << std::endl;
+				}				
+				break;
+			}
+		}
+	}
+	abs_end.close();
+	abs.close();
+	return 0;
+}
---
+++ sheet_3/conditional_absorb_new_initial_conditions/plots/plot.C
@@ -0,0 +1,65 @@
+//root plotting histogram for times
+void plot() {
+	std::ifstream f1("absorbed_end.dat");// determine min and max times
+	std::ifstream f2("absorbed.dat");
+	Double_t min1, max1, min2, max2, sum1, sum2 = 0;
+	Int_t entries1, entries2 = 0;
+	Double_t time;
+
+	while (!f1.eof()) {// read in data
+		f1 >> time;
+		if (time < min1) min1 = time;
+		if (time > max1) max1 = time;
+		sum1 += time;
+		++entries1;
+	}
+	
+	std::cout << "Mean end absorb time = " << (double) sum1 / entries1 << std::endl;
+
+	while (!f2.eof()) {
+		f2 >> time;
+		if (time < min2) min2 = time;
+		if (time > max2) max2 = time;
+		sum2 += time;
+		++entries2;
+	}
+	
+	std::cout << "Mean absorb time = " << (double) sum2 / entries2 << std::endl;	
+
+	f1.close();
+	f2.close();
+
+	TH1D* h1 = new TH1D("Conditional absorption", "Conditional absorption", 50, min1 - 100, max1 + 100);// declare histograms
+	TH1D* h2 = new TH1D("Unconditional absorption", "Unonditional absorption", 50, min2, max2);// declare histograms
+
+	std::ifstream f1("absorbed_end.dat");
+	std::ifstream f2("absorbed.dat");
+	Double_t time;
+
+	while (!f1.eof()) {// read in data
+		f1 >> time;
+		h1->Fill(time);
+	}
+
+	while (!f2.eof()) {
+		f2 >> time;
+		h2->Fill(time);
+	}
+	f1.close();
+	f2.close();
+
+	//canvases
+	TCanvas *c1 = new TCanvas("c1","Absorbtion time @ end",200,10,700,500);
+	h1->GetXaxis()->SetTitle("Absorption time");
+	h1->GetYaxis()->SetTitle("Entries / 50");	
+	h1->Draw();
+	//gStyle->SetOptStat(0);
+	c1->SaveAs("Absorbtion_time_at_end.png");
+
+	TCanvas *c2 = new TCanvas("c2","Absorbtion time",200,10,700,500);
+	h2->GetXaxis()->SetTitle("Absorption time");
+	h2->GetYaxis()->SetTitle("Entries / 50");	
+	h2->Draw();
+	//gStyle->SetOptStat(0);
+	c2->SaveAs("Absorbtion_time.png");	
+}
